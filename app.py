32ed8111aef566be410a3c21f3f13b260c3fff70
Karan Sharma <karansharma1295@gmail.com>
2016-11-22 16:03:26 +0530

    DB setup

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, request, redirect, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug import secure_filename
 
+cwd = os.getcwd()
+UPLOAD_FOLDER = cwd + '/upload/'
+ALLOWED_EXTENSIONS = set(['csv'])
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+print(os.environ['APP_SETTINGS'])
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def hello():
@@ -14,6 +26,27 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+@app.route("/upload", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            if not os.path.exists(UPLOAD_FOLDER):
+                os.makedirs(UPLOAD_FOLDER)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    <p>%s</p>
+    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
 
 if __name__ == '__main__':
     app.run()
