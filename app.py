4e671dd4f19fd83d2f33eb81c8abccccce249031
Karan Sharma <karansharma1295@gmail.com>
2016-11-23 11:42:19 +0530

    sqlalchemy setup

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
 import os
 from flask import Flask, request, redirect, url_for
-from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import secure_filename
+
+import sqlalchemy
+
+def connect(user, password, db, host='localhost', port=5432):
+    '''Returns a connection and a metadata object'''
+    # We connect with the help of the PostgreSQL URL
+    # postgresql://federer:grandestslam@localhost:5432/tennis
+    url = 'postgresql://{}:{}@{}:{}/{}'
+    url = url.format(user, password, host, port, db)
+
+    # The return value of create_engine() is our connection object
+    con = sqlalchemy.create_engine(url, client_encoding='utf8')
+
+    # We then bind the connection to MetaData()
+    meta = sqlalchemy.MetaData(bind=con, reflect=True)
+
+    return con, meta
+
+con, meta = connect('karan', 'karan', 'supplyai')
 
 cwd = os.getcwd()
 UPLOAD_FOLDER = cwd + '/upload/'
 ALLOWED_EXTENSIONS = set(['csv'])
-
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 print(os.environ['APP_SETTINGS'])
 
 def allowed_file(filename):
